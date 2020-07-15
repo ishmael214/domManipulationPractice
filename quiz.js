95f47436ac1bc6cff290f4a94ff435499288f460
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2020-07-15 11:12:10 -0500

    unfinished, moving on to next JS section of course

--- quiz.js
+++ quiz.js
@@ -4,6 +4,11 @@
         this.answerChoices = answerChoices;
         this.correctAnswer = correctAnswer;
     }
+
+    
+
+
+
     
     let q1 = new Quiz ('How many licks does it take to get to the tootsie roll center of a tootsie pop?',
                                                                           ['A: 7', 'B: Please Shut Up '],
@@ -15,70 +20,40 @@
                                                                  'A');
     
     
-    let q3 = new Quiz ('What are the odds of me pulling Ari Lennox',
+    let q3 = new Quiz ('What are the odds of me pulling Ari Lennox?',
                                     ['A: 100%', 'B: 50/50', 'C: 0'],
                                                                'B')
     
-    const showQuestionsVariable = Quiz.prototype.showQuestions = function () {
-        console.log('Question ' + (questionSelect+1) + '. ' + this.question);
     
+    Quiz.prototype.showQuestions = function () {
+        console.log('Question ' + (questionSelect+1) + '. ' + this.question);
          for (let i = 0; i < this.answerChoices.length; i++) {
              console.log(this.answerChoices[i]);
          }
          
     }
     
-    var nextQuestion = Quiz.prototype.rightAnswer = function () {
+    Quiz.prototype.rightAnswer = function () {
         if (answer === this.correctAnswer) {
             console.log('Correct! Now for the next question')
         } else {
             console.log('How did you get that wrong... go take a lap')
         }
     } 
-
-    /* var keepItGoing = Quiz.prototype.keepGoing = function () {
-        if (2+2 === 4) {
-            console.log('WORKING');
-        } else {
-            console.log('Not')
-        }
-
-    } 
-    */
     
     const quizQuestions = [q1, q2, q3]
     
-    const questionSelect = Math.floor(Math.random(quizQuestions) * quizQuestions.length);
-    
-    console.log(questionSelect);
-    
-    /* function something() {
-        for (i = 0; i < quizQuestions.length; i++) {
-            console.log(quizQuestions[i*questionSelect]);
-        }
-    
-    }
-    
-    something(); */
-    
-    quizQuestions[questionSelect].showQuestions();
-    
-    let answer = prompt('Enter in the correct answer choice (CASE SENSITIVE)')
-    
-    quizQuestions[questionSelect].rightAnswer();
 
-    let score = 0;
-    
- /*   Quiz.prototype.mathFunction = () => {
-        if (nextQuestion) {
-        showQuestionsVariable();
-        score += 1
-        console.log('Score: ' + score);
-        } else {
-            console.log('NOT WORKING');
+    function nextQuestion () {   
+        const questionSelect = Math.floor(Math.random(quizQuestions) * quizQuestions.length);                                
+        quizQuestions[questionSelect].showQuestions();                                                
+        let answer = prompt('Enter in the correct answer choice (CASE SENSITIVE)')                                                
+        quizQuestions[questionSelect].rightAnswer();
+                                                            
         }
-    }
+        
+    nextQuestion();
     
-    mathFunction(); */
     
+
 })();
